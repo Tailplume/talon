c70e4aeb58525574596b2f4ed9980c7ddd2d5e84
Aurorah Harmony <aurorahHarmony@gmail.com>
2023-05-12 00:20:29 +1200

    Make registration input more responsive

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -27,22 +27,22 @@ import { RouterLink } from "vue-router";
         alt="Be yourself"
         class="w-10/12 md:w-9/12 lg:w-7/12 xl:w-5/12 mb-3"
       />
-      <p class="text-2xl mb-5">
+      <p class="text-lg md:text-2xl mb-5">
         Everything you make, share and sell online,<br />
         all in one place
       </p>
       <form>
         <div
           name="link"
-          class="px-6 py-4 mr-4 bg-white/[.16] rounded-r-2xl border-black/[0.4] border-2 inline text-xl font-light opacity-60"
+          class="px-6 py-4 mr-4 mb-2 bg-white/[.16] rounded-r-2xl border-black/[0.4] border-2 inline-flex items-center text-xl font-light opacity-60"
           style="border-top-left-radius: 2rem; border-bottom-left-radius: 2rem"
         >
-          tailplu.me/
-          <input type="text" class="bg-transparent w-44" disabled />
+          <span>tailplu.me/</span>
+          <input type="text" class="bg-transparent w-44 ml-2" disabled />
         </div>
 
         <button
-          class="bg-white/[.16] px-6 py-4 rounded-full hover:bg-white/[0.25] font-medium duration-200 text-xl disabled:opacity-50 disabled:pointer-events-none relative"
+          class="bg-white/[.16] px-6 py-4 rounded-full hover:bg-white/[0.25] font-medium duration-200 inline-block text-xl disabled:opacity-50 disabled:pointer-events-none relative"
           disabled
         >
           Claim your tailplume
